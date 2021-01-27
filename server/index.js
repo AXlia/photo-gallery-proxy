77a83b5be3fc706d1777e6a3e7ebc6adf9eb1686
Adam Brent <adam.brent@gmail.com>
2021-01-26 23:46:07 -0800

    Adds http protocol to localhost links in proxy middleware. Updates references to bundles in index.html (WIP).

--- server/index.js
+++ server/index.js
@@ -2,20 +2,16 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-
-app.use(express.static(__dirname + '/../client/public'));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/api/properties', properties.get);
-// app.patch('/api/properties/:propertyId', properties.patch);
-// app.get('/api/photos/:propertyId', photos.get);
-
-const carouselService = createProxyMiddleware({ target: 'localhost:3001', changeOrigin: true })
-const affordability = createProxyMiddleware({ target: 'localhost:3002', changeOrigin: true })
-const photoGallery = createProxyMiddleware({ target: 'localhost:3003', changeOrigin: true })
+const carouselService = createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true })
+const affordability = createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true })
+const photoGallery = createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true })
 
 app.use(['/api/homes', '/api/new', '/api/similar'], carouselService);
 app.use(['/api/home_price', '/all_rates'], affordability);
