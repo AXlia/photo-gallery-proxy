d33f87944a033f16592e7919d509808affbe42a1
Adam Brent <adam.brent@gmail.com>
2021-01-26 16:52:24 -0800

    Adds createHttpProxy routing for the three services.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
+
 
 app.use(express.static(__dirname + '/../client/public'));
 
@@ -11,9 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 // app.patch('/api/properties/:propertyId', properties.patch);
 // app.get('/api/photos/:propertyId', photos.get);
 
+const carouselService = createProxyMiddleware({ target: 'localhost:3001', changeOrigin: true })
+const affordability = createProxyMiddleware({ target: 'localhost:3002', changeOrigin: true })
+const photoGallery = createProxyMiddleware({ target: 'localhost:3003', changeOrigin: true })
 
-
-
+app.use(['/api/homes', '/api/new', '/api/similar'], carouselService);
+app.use(['/api/home_price', '/all_rates'], affordability);
+app.use(['/api/photos', '/api/properties'], photoGallery);
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
