5f741f53d1d605d34f8528eae5af3b86b4b016a9
Adam Brent <adam.brent@gmail.com>
2021-01-27 12:42:11 -0800

    Fixes linter config. Linting pass.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 const port = 3000;
 const path = require('path');
@@ -9,14 +10,14 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const carouselService = createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true })
-const affordability = createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true })
-const photoGallery = createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true })
+const carouselService = createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true });
+const affordability = createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true });
+const photoGallery = createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true });
 
 app.use(['/api/homes', '/api/new', '/api/similar'], carouselService);
 app.use(['/api/home_price', '/all_rates'], affordability);
 app.use(['/api/photos', '/api/properties'], photoGallery);
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
