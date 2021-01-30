de3ee2aba9db19a2273455f2d2028f14cb6a78f6
Adam Brent <adam.brent@gmail.com>
2021-01-30 14:50:00 -0800

    Updates server index to point to AWS EC IP addresses for services.

--- server/index.js
+++ server/index.js
@@ -8,15 +8,13 @@ const path = require('path');
 
 app.use('/:propertyId', express.static(path.join(__dirname, '../client/public')));
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const carouselService = createProxyMiddleware({ target: 'localhost:3001', changeOrigin: true });
 const carouselService = createProxyMiddleware({ target: 'http://54.183.174.156:3001', changeOrigin: true });
-const affordability = createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true });
-const photoGallery = createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true });
+const affordability = createProxyMiddleware({ target: 'http://3.140.200.255:3002', changeOrigin: true });
+const photoGallery = createProxyMiddleware({ target: 'http://54.186.33.211:3003', changeOrigin: true });
 
 app.use(['/api/homes', '/api/new', '/api/similar'], carouselService);
 app.use(['/api/home_price', '/all_rates'], affordability);
